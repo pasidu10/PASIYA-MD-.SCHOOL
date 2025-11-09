c1fa01e5114e8499457ae06ffb5e93baab64884b
pasidu10 <punnyanamda.thero@gmail.com>
2025-11-09 20:11:17 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from "react";
+import Home from "./components/Home";
+import Registration from "./components/Registration";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
